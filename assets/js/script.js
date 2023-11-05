b660b6ead8b6aa21a2b4ee48e714de9f57a9d05e
Faysal-Ahmed-Aweys <>
2023-11-05 17:43:17 +0000

    Add start button functionality to display username-form page

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let startBtn = document.getElementById('start-button');
+    startBtn.addEventListener('click', displayUsernameForm);
+});
+
+function displayUsernameForm() {
+    let homePage = document.getElementById('home-page');
+    let formSection = document.getElementById('form-section');
+    homePage.classList.add('hidden');
+    formSection.classList.remove('hidden');
+    formSection.addEventListener('submit', function (event) {
+        event.preventDefault();
+        displayDifficultyPage();
+    });
+};
